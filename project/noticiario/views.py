f695efddf6a09ec7d1e092cfb0ec9a5b53de9292
Luciano Ramalho <luciano@ramalho.org>
2012-06-22 10:37:59 -0300

    melhoria na legibilidade da HomePageView

--- project/noticiario/views.py
+++ project/noticiario/views.py
@@ -16,22 +16,23 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         secao = kwargs.get('secao')
         context = super(HomePageView, self).get_context_data(**kwargs)
+        destaques = Noticia.objects.filter(destaque=1)
+        if secao is not None:
+            destaques = destaques.filter(secao=secao)
         try:
-            destaques = Noticia.objects.filter(destaque=1)
-            if secao:
-                destaques = destaques.filter(secao=secao)
             destaque = destaques.latest()
         except Noticia.DoesNotExist:
             destaque = {'titulo':u'Nenhum destaque primário encontrado',
-                        'lead':u'Use o admin para designar uma notícia'
+                        'lead':u'Carregue uma fixture de noticiario/fixtures'
+                                 u' e use o admin para designar uma notícia'
                                  u' como destaque primário. O destaque'
                                  u' primário mais recente aparece aqui',
                         'pk': 0}
         context['destaque'] = destaque
         resultado = Noticia.objects.filter(destaque=2)
-        if secao:
+        if secao is not None:
             resultado = resultado.filter(secao=secao)
-        resultado = resultado.order_by('dt_criacao')[:12]
+        resultado = resultado.order_by('-dt_criacao')[:12]
         context['destaques_secundarios'] = (resultado[i:i+3] for i in range(0, len(resultado), 3))
         context['secoes'] = menu_secoes(secao)
         return context
